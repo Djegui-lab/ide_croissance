ed871f404ab450836221d644bc69ffc763803a10
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2023-06-26 21:48:40 +0200

    req3 commit

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-import streamlit as st
-import pandas as pd
 import plotly.express as px
 import streamlit as st
 import pandas as pd
-import plotly.express as px
 
 st.write("hello world")
 st.title('VISUALISATION')
