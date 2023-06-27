6b1364844c8443c577d5448cb02316678d1a7661
Djegui-lab <110122323+Djegui-lab@users.noreply.github.com>
2023-06-27 14:24:49 +0200

    ajout de l'investissement

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import plotly.express as px
 import streamlit as st
 import pandas as pd
+from openpyxl import Workbook
 
 st.write("hello world")
 st.title('VISUALISATION')
@@ -14,20 +15,20 @@ if uploaded_file:
 
     groupby_column = st.selectbox(
         'What would you like to analyse?',
-        ('annees','investissement', 'Croissance du PIB (% annuel)','IDE_sorties_nettes ','Taux de change ','Chomage ', "code_pays", "Exportations de biens et services "),
+        ('annees',"investissement", "Croissance du PIB (% annuel)",'IDE_sorties_nettes ','Taux de change ','Chomage ', "code_pays", "Exportations de biens et services "),
     )
     # -- GROUP DATAFRAME
-    output_columns = ['Croissance du PIB (% annuel)', 'Exportations de biens et services ']
+    output_columns = ["Croissance du PIB (% annuel)", "investissement"]
     df_grouped = df.groupby(by=[groupby_column], as_index=False)[output_columns].mean()
     st.dataframe(df_grouped)
     # -- PLOT DATAFRAME
     fig = px.bar(
         df_grouped,
         x=groupby_column,
-        y='Croissance du PIB (% annuel)',
-        color='Exportations de biens et services ',
+        y='code_pays',
+        color="Croissance du PIB (% annuel)",
         color_continuous_scale=['red', 'yellow', 'green'],
         template='plotly_white',
-        title=f'<b>Croissance du PIB (% annuel) & Exportations de biens et services  by {groupby_column}</b>'
+        title=f'<b>Croissance du PIB (% annuel) & investissement  by {groupby_column}</b>'
     )
     st.plotly_chart(fig)
